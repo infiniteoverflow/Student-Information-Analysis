a5f13ca0269ed62b79f0fec5c60ba58e5f7bcd0c
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-10-28 21:40:04 +0530

    Added login.py

---
+++ login.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+class Login:
+    def __init__(self):
+        # Defining the root window
+        self.root = Tk()
+        self.root.geometry("800x800")
+        
+        # Defining the canvas on which the widgets has to be placed
+        self.c = Canvas(self.root,bg = "gray",height=800,width=800,cursor='pencil')
+        
+        # Setting the background image
+        image = Image.open("images/login_bg.jpg")
+        photo = ImageTk.PhotoImage(image)
+        
+        self.c.create_image((0,0), image=photo, anchor="nw")
+        self.c.create_text((100, 150), text="Student Information Analysis", fill="black", anchor="nw"
+                           ,font=('newcenturyschlbk',30,'bold'))
+        
+        # Defining the window
+        self.c.pack()
+        self.root.mainloop()
+
+c = Login()
