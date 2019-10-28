7d010e3de87af91270d83b7fa5eb3101f27b84e1
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-10-28 22:51:40 +0530

    Updated login.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 import login
 
 login_page = login.Login()
+login_page.credentials()
--- login.py
+++ login.py
@@ -6,6 +6,7 @@ class Login:
         # Defining the root window
         self.root = Tk()
         self.root.geometry("800x800")
+        self.root.title("Login")
         
         # Defining the canvas on which the widgets has to be placed
         self.c = Canvas(self.root,bg = "gray",height=800,width=800,cursor='pencil')
@@ -16,13 +17,44 @@ class Login:
         
         # Setting the background
         self.c.create_image((0,0), image=photo, anchor="nw")
+        self.f = Frame(self.root,height=800,width=800)
+        
+        # Setting the font
+        self.fnt = ('latin modern typewriter',50,'bold')
         
         # Setting the text
         self.c.create_text((100, 150), text="Student Information Analysis", fill="black", anchor="nw"
                            ,font=('newcenturyschlbk',30,'bold'))
         self.c.create_text((280, 270), text="Login", fill="black", anchor="nw"
-                           ,font=('latin modern typewriter',50,'bold'))
+                           ,font=self.fnt)
+        
+        # Setting the login entry boxes
+        self.c.create_text((190, 390), text="Username: ", fill="black", anchor="nw"
+                           ,font=('Times',30,'italic bold'))
+        
+        self.button1 = Entry(self.c,font=('Times',20,'bold'))
+        self.button1.configure(width = 20, relief = FLAT)  
+        button1_window = self.c.create_window(380, 400, anchor=NW, window=self.button1)
+        
+        
+        self.c.create_text((190, 480), text="Password: ", fill="black", anchor="nw"
+                           ,font=('Times',30,'italic bold'))
+        
+        self.button2 = Entry(self.c,font=('Times',20,'bold'),show="*")
+        self.button2.configure(width = 20, relief = FLAT)  
+        button1_window = self.c.create_window(380, 490, anchor=NW, window=self.button2)
+        
+        self.button2.bind("<Return>",self.credentials)
         
         # Defining the window
         self.c.pack()
         self.root.mainloop()
+        
+    def credentials(self,event):
+        username = self.button1.get()
+        password = self.button2.get()
+        
+        print(username)
+        print(password)
+        
+
