f2a8964a78e6b31adcaf8fbf5976a44ae5014aea
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-11-21 14:08:38 +0530

    Added Highest package code

---
+++ screens/Highest.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import pandas as pd
+
+class Highest:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("700x700")
+        self.root.title("Placement Details for {}".format('IEM'))
+
+        df = pd.read_csv('./databases/placement.csv')
+
+        a = 'CSE'
+        a_df = df[df['BRANCH']==a]
+        a_df
+
+        x = a_df['COMPANY_NAME']
+        y = a_df['HIGHEST_PACKAGE']
+
+        df1 = a_df[['COMPANY_NAME','HIGHEST_PACKAGE']] \
+            .groupby('COMPANY_NAME').sum()
+        
+        figure1 = plt.Figure(figsize=(7,7), dpi=70)
+        ax1 = figure1.add_subplot(111)
+        bar1 = FigureCanvasTkAgg(figure1, self.root)
+
+        bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+        df1.plot(kind='bar', legend=True, ax=ax1)
+        ax1.set_title('Companies Visited Vs. Total Students Appeared and Placed')
+
+        self.root.mainloop()
+
+a = CvsTa()
