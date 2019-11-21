0628fe6de523d997373ea77649c7a55348386aec
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-11-21 07:11:22 +0530

    Added tkinter matplotlib support

---
+++ tkinter-mat.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+import pandas as pd
+
+df = pd.read_csv('./databases/placement.csv')
+
+a = 'ECE'
+
+a_df = df[df['BRANCH']==a]
+tot_app = a_df['TOTAL_APPEARED']
+tot_plac = a_df['TOTAL_PLACED']
+x = a_df['COMPANY_NAME']
+
+df1 = a_df[['COMPANY_NAME', 'TOTAL_APPEARED','TOTAL_PLACED']].groupby('COMPANY_NAME').sum()
+
+print(a_df)
+
+root= tk.Tk() 
+root.geometry("600x600")
+  
+
+figure1 = plt.Figure(figsize=(7,7), dpi=70)
+ax1 = figure1.add_subplot(111)
+bar1 = FigureCanvasTkAgg(figure1, root)
+
+bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+df1.plot(kind='bar', legend=True, ax=ax1)
+ax1.set_title('Companies Visited Vs. Total Students Appeared and Placed')
+
+root.mainloop()
